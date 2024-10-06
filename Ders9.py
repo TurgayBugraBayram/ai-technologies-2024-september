17bbd3c9177e4616c34781b36e5926013c6abc40
TurgayBugraBayram <turgaybugrabayram@gmail.com>
2024-10-06 19:43:31 +0300

    tryexcept, dosya işlemeri, pip

--- Ders9.py
+++ Ders9.py
@@ -1,1 +1,55 @@
-print("merhaba")
+# try:
+#     a = int(input("sayı giriniz : "))
+#     print(a)
+#
+# # except ValueError:
+# #     print("value error geldi")
+# #
+# # except NameError:
+# #     print("")
+#
+# except Exception as e:
+#     print("hata oluştu",e)
+#
+# else:
+#     print("hata gelmedi")
+# finally:
+#     print("finally")
+#
+# print("merhaba")
+#
+# x = "kesif"
+#
+# if x == "kesif":
+#   raise TypeError("hata geldi")
+#
+# print("24.satır")
+
+# file = open("bilgileri_göster.txt","w")
+#
+# file.write("kesifplus")
+#
+# file.close()
+
+
+# with open("bilgileri_göster.txt","w",encoding="utf-8") as file:
+#     file.write("buğra")
+
+# with open("bilgileri_göster.txt","a",encoding="utf-8") as file:
+#     file.write("\nahmet")
+#     file.write("merhaba")
+
+# with open("bilgileri_göster.txt","r",encoding="utf-8") as file:
+#     # for i in file:
+#     #     print(i)
+#     # print(file.read())
+#     # print(file.readline())
+#     # print(file.readline())
+#     # print(file.readline())
+#
+#     print(file.readlines())
+
+
+from time import sleep
+import random as rd
+
